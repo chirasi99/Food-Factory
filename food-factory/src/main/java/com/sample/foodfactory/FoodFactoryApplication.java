f5e7716387b2d9f2d0a9af7b484e0314f6cbc9a5
Chirasi Amaya Walpola <2020cs197@stu.ucsc.cmb.ac.lk>
2024-05-27 11:58:54 +0530

    initial commit

---
+++ food-factory/src/main/java/com/sample/foodfactory/FoodFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.sample.foodfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFactoryApplication.class, args);
+	}
+
+}
